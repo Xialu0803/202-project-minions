520a9d8df5a3cd62c81572ef461c805edb6d65cd
Xialu0803 <xialu.zou@sjsu.edu>
2022-11-16 02:36:32 -0800

    add

--- server/index.js
+++ server/index.js
@@ -2,17 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import path from 'path';
 
 import flightRoutes from './routes/flights.js';
 import baggageRoutes from './routes/baggages.js';
 import gateRoutes from './routes/gates.js';
 import userRoutes from './routes/users.js';
-import dotenv from "dotenv";
 
 
 const app = express();
-const __dirname = path.resolve();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -27,12 +24,7 @@ app.use('/users', userRoutes);
 const CONNECTION_URL = 'mongodb+srv://mern:javascript@cluster0.mrm6pyx.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    });
-   }
+
 
 
    mongoose.connect(process.env.MONGODB_URI||CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
